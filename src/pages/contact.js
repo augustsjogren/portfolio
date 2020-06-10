c323ad6c6871143a43e6d00f1128b13ec3a7c250
August Sjögren <contact@augustsjogren.com>
2020-06-10 21:35:57 +0200

    Redesign of contact page (#5)

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,7 +7,6 @@ import {
   FaEnvelope,
   FaMapMarkerAlt,
 } from 'react-icons/fa';
-import { GiMailbox } from 'react-icons/gi';
 
 import { Container, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -30,76 +29,76 @@ const ContactPage = () => (
           className='mx-4'
         >
           <h1>Contact</h1>
-          <p>
-            Want to get in touch? Feel free to send me an email or contact me
-            using any of the other ways listed!
+          <p className='mb-4'>
+            Want to get in touch? Feel free to send me an email or message me at
+            LinkedIn!
           </p>
-          <div>
-            <Row className='mt-2'>
-              <Col md className='my-md-0 my-2'>
-                <a
-                  href='mailto:contact@augustsjogren.com'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  <div className='d-flex flex-row align-items-center'>
-                    <FaEnvelope
-                      color='black'
-                      size='32px'
-                      style={{ marginRight: '15px' }}
-                    />
-                    <span>Email</span>
-                  </div>
-                </a>
-              </Col>
-              <Col md className='my-md-0 my-2'>
-                <a
-                  href='https://www.linkedin.com/in/august-sjogren/'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  <div className='d-flex flex-row align-items-center'>
-                    <FaLinkedin
-                      color='black'
-                      size='32px'
-                      style={{ marginRight: '15px' }}
-                    />
-                    <span>LinkedIn</span>
-                  </div>
-                </a>
-              </Col>
-              <Col md className='my-md-0 my-2'>
-                <a
-                  href='https://github.com/augustsjogren'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  <div className='d-flex flex-row align-items-center'>
-                    <FaGithub
-                      color='black'
-                      size='32px'
-                      style={{ marginRight: '15px' }}
-                    />
-                    <span>GitHub</span>
-                  </div>
-                </a>
-              </Col>
-            </Row>
-          </div>
+          <p className='mb-2'>
+            <a
+              href='mailto:contact@augustsjogren.com'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <span>
+                <FaEnvelope
+                  color='black'
+                  size='24px'
+                  style={{ marginRight: '15px' }}
+                />
+              </span>
+              <span>contact@augustsjogren.com</span>
+            </a>
+          </p>
+          <p className='mb-2'>
+            <a
+              href='https://www.linkedin.com/in/august-sjogren'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <span>
+                <FaLinkedin
+                  color='black'
+                  size='24px'
+                  style={{ marginRight: '15px' }}
+                />
+              </span>
+              <span>linkedin.com/in/august-sjogren</span>
+            </a>
+          </p>
+          <p>
+            <a
+              href='https://github.com/augustsjogren'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <span>
+                <FaGithub
+                  color='black'
+                  size='24px'
+                  style={{ marginRight: '15px' }}
+                />
+              </span>
+              <span>github.com/augustsjogren</span>
+            </a>
+          </p>
         </Col>
         <Col
-          md={true}
-          className=' d-flex justify-content-center align-items-center mx-4 mt-4 mt-md-0'
+          md
+          className='d-flex d-md-flex flex-column justify-content-center align-items-center mx-4 mt-4 mt-md-0'
         >
-          <div
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
-          >
-            <GiMailbox size={128} />
-          </div>
+          <Row>
+            <Col>
+              <FaMapMarkerAlt className='d-none d-md-block' size='120' />
+              <FaMapMarkerAlt className='d-block d-md-none' size='80' />
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <p className='mt-4' style={{ fontSize: '1.5rem' }}>
+                Linköping, SE
+              </p>
+            </Col>
+          </Row>
         </Col>
       </Row>
     </Container>
