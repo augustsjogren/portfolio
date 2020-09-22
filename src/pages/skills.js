c2516dc6f84dea0c7ccbe924ee669aa771c9429b
August Sjögren <august.sjogren@gmail.com>
2020-09-22 22:14:56 +0200

    Fix casing of skillTag import

--- src/pages/skills.js
+++ src/pages/skills.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
-import SkillTag from '../components/SkillTag';
+import SkillTag from '../components/skillTag';
 
 const skills = [
   'React.js',
