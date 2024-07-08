f5b7d99ee0f4ed3ab1f8cd94b95d64391b338d9f
August Sjögren <august.sjogren@gmail.com>
2024-07-08 21:50:25 +0200

    clean up package json

--- src/components/skillTag.tsx
+++ src/components/skillTag.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const SkillTag = props => {
+const SkillTag = (props) => {
   return (
     <div
       style={{
@@ -11,7 +11,7 @@ const SkillTag = props => {
         paddingBottom: '10px',
         border: '2px solid black',
         borderRadius: '50px',
-        color: '#000000'
+        color: '#000000',
       }}
     >
       {props.content}
