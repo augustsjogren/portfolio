e55386b211cffd9bbe0cc11fa498e5e360792628
August Sjögren <august.sjogren@gmail.com>
2024-07-09 20:57:52 +0200

    optimize image

--- src/components/image.tsx
+++ src/components/image.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import profileImage from '../images/Aggebagge_web.jpg';
+import profileImage from '../images/Aggebagge_web.webp';
 
 const BioImage = () => {
   return <img src={profileImage} />;
