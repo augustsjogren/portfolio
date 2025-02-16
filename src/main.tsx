a1cc9e84bfc4182e93f09651a449484d4005213f
August Sjögren <hello@augustsjogren.com>
2025-02-16 13:48:35 +0100

    adjust base path in router

--- src/main.tsx
+++ src/main.tsx
@@ -10,18 +10,21 @@ import AboutPage from './pages/about';
 import ContactPage from './pages/contact';
 import SkillsPage from './pages/skills';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout />,
-    children: [
-      { element: <IndexPage />, index: true },
-      { path: 'about', element: <AboutPage /> },
-      { path: 'skills', element: <SkillsPage /> },
-      { path: 'contact', element: <ContactPage /> },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        { element: <IndexPage />, index: true },
+        { path: 'about', element: <AboutPage /> },
+        { path: 'skills', element: <SkillsPage /> },
+        { path: 'contact', element: <ContactPage /> },
+      ],
+    },
+  ],
+  { basename: '/portfolio' }
+);
 
 const domNode = document.getElementById('root') as HTMLElement;
 const root = ReactDOM.createRoot(domNode);
